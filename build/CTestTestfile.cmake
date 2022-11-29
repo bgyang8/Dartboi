e0ee22c9bfc1ea718e041375aac8e8ed421cc967
Class Account <ee106a-abl@c105-6.local>
2022-11-29 14:50:11 -0800

    head camera info in '_logs'

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-abl/Dartboi/src
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-abl/Dartboi/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ar_track_alvar/ar_track_alvar_msgs")
+subdirs("ar_track_alvar/ar_track_alvar")
+subdirs("tracking")
