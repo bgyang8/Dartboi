2ec6724c16eeca8bb10fcdd2ad90411b982bc83f
kaiyun717 <kaiyun717@gmail.com>
2022-11-30 14:04:28 -0800

    tabs

--- src/tracking/src/transformation.py
+++ src/tracking/src/transformation.py
@@ -14,83 +14,82 @@ from geometry_msgs.msg import Twist, Vector3
 
 from geometry_msgs.msg import Twist
 
-#Define the method which contains the main functionality of the node.
 def get_transform(frame1, frame2, traj_dist):
-  """
-  Controls a turtlebot whose position is denoted by frame1,
-  to go to a position denoted by target_frame
-  Inputs:
-  - frame1: the tf frame of the AR tag on your turtlebot
-  - target_frame: the tf frame of the target AR tag
-  """
-
-  #Create a publisher and a tf buffer, which is primed with a tf listener
-  pub = rospy.Publisher('launch_des', Twist, queue_size=10)
-  tfBuffer = tf2_ros.Buffer()
-  tfListener = tf2_ros.TransformListener(tfBuffer)
+    """
+    Controls a turtlebot whose position is denoted by frame1,
+    to go to a position denoted by target_frame
+    Inputs:
+    - frame1: the tf frame of the AR tag on your turtlebot
+    - target_frame: the tf frame of the target AR tag
+    """
   
-  # Create a timer object that will sleep long enough to result in
-  # a 10Hz publishing rate
-  r = rospy.Rate(10) # 10hz
-
-  # Loop until the node is killed with Ctrl-C, traj_dist
-  while not rospy.is_shutdown():
-    try:
-        # The transform of the target with respect to the head camera
-      trans = tfBuffer.lookup_transform(frame2, frame1, rospy.Time())
-    #   print(trans)
-    #   # Process trans to get your state error
-      x = trans.transform.translation.x
-      y = trans.transform.translation.y
-      z = trans.transform.translation.z
-
-      quat = trans.transform.rotation
-
-      roll, pitch, yaw = quat_to_rpy(quat)
+    #Create a publisher and a tf buffer, which is primed with a tf listener
+    pub = rospy.Publisher('launch_des', Twist, queue_size=10)
+    tfBuffer = tf2_ros.Buffer()
+    tfListener = tf2_ros.TransformListener(tfBuffer)
     
-    #   Testing direction
-    #   print('R: ', roll / np.pi * 180)
-    #   print('P: ', pitch / np.pi * 180)
-    #   print('Y: ', yaw / np.pi * 180)
-
-    # Experimentally, PITCH is the horizontal "yaw" of the target
-    # Flat is 0 deg, ccw is positive, cw is negative
-      theta = pitch
-
-      x_d = traj_dist * np.cos(theta)
-      y_d = y
-      z_d = z - traj_dist * np.sin(theta)
-
-      r_des = 0
-      p_des = 0
-      y_des = theta
-
-    #   # Generate a control command to send to the robot
-      launch_twist = Twist()
-      launch_twist.linear.x = x_d
-      launch_twist.linear.y = y_d
-      launch_twist.linear.z = z_d
-      launch_twist.angular.x = r_des
-      launch_twist.angular.y = p_des
-      launch_twist.angular.z = y_des
-      
-      pub.publish(launch_twist)
-      print(launch_twist)
-
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-      pass
-    # Use our rate object to sleep until it is time to publish again
-    r.sleep()
-
-
-"""Get rpy from Quaternion"""
+    # Create a timer object that will sleep long enough to result in
+    # a 10Hz publishing rate
+    r = rospy.Rate(10) # 10hz
+  
+    # Loop until the node is killed with Ctrl-C, traj_dist
+    while not rospy.is_shutdown():
+        try:
+            # The transform of the target with respect to the head camera
+            trans = tfBuffer.lookup_transform(frame2, frame1, rospy.Time())
+            
+            # Process trans to get your state error
+            x = trans.transform.translation.x
+            y = trans.transform.translation.y
+            z = trans.transform.translation.z
+    
+            quat = trans.transform.rotation
+    
+            roll, pitch, yaw = quat_to_rpy(quat)
+        
+            # Testing direction
+            # print('R: ', roll / np.pi * 180)
+            # print('P: ', pitch / np.pi * 180)
+            # print('Y: ', yaw / np.pi * 180)
+    
+            # Experimentally, PITCH is the horizontal "yaw" of the target
+            # Flat is 0 deg, ccw is positive, cw is negative
+            theta = pitch
+    
+            x_d = traj_dist * np.cos(theta)
+            y_d = y
+            z_d = z - traj_dist * np.sin(theta)
+    
+            r_des = 0
+            p_des = 0
+            y_des = theta
+    
+            # Generate a control command to send to the robot
+            launch_twist = Twist()
+            launch_twist.linear.x = x_d
+            launch_twist.linear.y = y_d
+            launch_twist.linear.z = z_d
+            launch_twist.angular.x = r_des
+            launch_twist.angular.y = p_des
+            launch_twist.angular.z = y_des
+            
+            pub.publish(launch_twist)
+            print(launch_twist)
+  
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            pass
+        
+        # Use our rate object to sleep until it is time to publish again
+        r.sleep()
+  
+  
+""  "Get rpy from Quaternion"""
 def quat_to_rpy(q):
     rpy = [0, 0, 0]
 
     sinr_cosp = 2 * (q.w * q.x + q.y * q.z)
     cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y)
     rpy[0] = np.arctan2(sinr_cosp, cosr_cosp)
-
 
     sinp = 2 * (q.w * q.y - q.z * q.x)
     if np.abs(sinp) >= 1:
@@ -115,14 +114,12 @@ def trans_node(name, frame1, frame2, traj_dist, anonymous=True):
         pass
 
       
-# This is Python's sytax for a main() method, which is run by default
-# when exectued in the shell
 if __name__ == '__main__':
-  # Check if the node has received a signal to shut down
-  # If not, run the talker method
-
-  #Run this program as a new node in the ROS computation graph 
-  #called /turtlebot_controller.
+    # Check if the node has received a signal to shut down
+    # If not, run the talker method
+  
+    #Run this program as a new node in the ROS computation graph 
+    #called /turtlebot_controller.
     frame1 = 'head_camera'
     frame2 = 'ar_marker_6'
     traj_dist = 1  # [meters]
