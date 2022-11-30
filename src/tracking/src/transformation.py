7c5a030695bb4b59b0349821b053904d619728ca
Class Account <ee106a-abl@c105-9.local>
2022-11-30 12:28:34 -0800

    Launcher TF twist is working.

--- src/tracking/src/transformation.py
+++ src/tracking/src/transformation.py
@@ -15,7 +15,7 @@ from geometry_msgs.msg import Twist, Vector3
 from geometry_msgs.msg import Twist
 
 #Define the method which contains the main functionality of the node.
-def get_transform(frame1, frame2):
+def get_transform(frame1, frame2, traj_dist):
   """
   Controls a turtlebot whose position is denoted by frame1,
   to go to a position denoted by target_frame
@@ -25,7 +25,7 @@ def get_transform(frame1, frame2):
   """
 
   #Create a publisher and a tf buffer, which is primed with a tf listener
-  pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+  pub = rospy.Publisher('launch_des', Twist, queue_size=10)
   tfBuffer = tf2_ros.Buffer()
   tfListener = tf2_ros.TransformListener(tfBuffer)
   
@@ -33,45 +33,84 @@ def get_transform(frame1, frame2):
   # a 10Hz publishing rate
   r = rospy.Rate(10) # 10hz
 
-  # Loop until the node is killed with Ctrl-C
+  # Loop until the node is killed with Ctrl-C, traj_dist
   while not rospy.is_shutdown():
     try:
         # The transform of the target with respect to the head camera
       trans = tfBuffer.lookup_transform(frame2, frame1, rospy.Time())
-      print(trans)
+    #   print(trans)
     #   # Process trans to get your state error
       x = trans.transform.translation.x
       y = trans.transform.translation.y
       z = trans.transform.translation.z
 
-    # Planar distance in the x-z (horizontal) plane
-      dist = np.sqrt(x**2 + z**2)
+      quat = trans.transform.rotation
 
-    # Want to the same y as the target
-      y_to_go_to = y
+      roll, pitch, yaw = quat_to_rpy(quat)
+    
+    #   Testing direction
+    #   print('R: ', roll / np.pi * 180)
+    #   print('P: ', pitch / np.pi * 180)
+    #   print('Y: ', yaw / np.pi * 180)
 
-    # Roll and pitch zero
-      r = 0
-      p = 0
+    # Experimentally, PITCH is the horizontal "yaw" of the target
+    # Flat is 0 deg, ccw is positive, cw is negative
+      theta = pitch
 
-    # yaw is ...
-      y = ''
+      x_d = traj_dist * np.cos(theta)
+      y_d = y
+      z_d = z - traj_dist * np.sin(theta)
 
+      r_des = 0
+      p_des = 0
+      y_des = theta
 
     #   # Generate a control command to send to the robot
-    #   msg = Twist()
+      launch_twist = Twist()
+      launch_twist.linear.x = x_d
+      launch_twist.linear.y = y_d
+      launch_twist.linear.z = z_d
+      launch_twist.angular.x = r_des
+      launch_twist.angular.y = p_des
+      launch_twist.angular.z = y_des
+      
+      pub.publish(launch_twist)
+      print(launch_twist)
 
     except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
       pass
     # Use our rate object to sleep until it is time to publish again
     r.sleep()
 
+
+"""Get rpy from Quaternion"""
+def quat_to_rpy(q):
+    rpy = [0, 0, 0]
+
+    sinr_cosp = 2 * (q.w * q.x + q.y * q.z)
+    cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y)
+    rpy[0] = np.arctan2(sinr_cosp, cosr_cosp)
+
+
+    sinp = 2 * (q.w * q.y - q.z * q.x)
+    if np.abs(sinp) >= 1:
+        rpy[1] = (np.pi / 2) * np.sign(sinp)
+    else:
+        rpy[1]= np.arcsin(sinp)
+
+    siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+    cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
+    rpy[2] = np.arctan2(siny_cosp, cosy_cosp)
+
+    return rpy
+
+
 """Get transformations between frame1 (reference) and frame2 (target)"""
-def trans_node(name, frame1, frame2, anonymous=True):
+def trans_node(name, frame1, frame2, traj_dist, anonymous=True):
     rospy.init_node(name)
 
     try:
-        get_transform(frame1, frame2)
+        get_transform(frame1, frame2, traj_dist)
     except rospy.ROSInterruptException:
         pass
 
@@ -86,6 +125,7 @@ if __name__ == '__main__':
   #called /turtlebot_controller.
     frame1 = 'head_camera'
     frame2 = 'ar_marker_6'
+    traj_dist = 1  # [meters]
     name = 'ar_transform'
-    trans_node(name, frame1, frame2)
+    trans_node(name, frame1, frame2, traj_dist)
   
